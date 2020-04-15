82d69908854418ef60ed7334b9f92a7ebb0258a0
HsiuKangHuang <k2025242322@gmail.com>
2020-04-15 16:19:24 -0400

    add delay between the edirect commands to avoid reaching NCBI requesting limit

--- rnannot/download_sra_metadata.py
+++ rnannot/download_sra_metadata.py
@@ -1,5 +1,6 @@
 import subprocess
 import argparse
+import time
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-t","--taxid", help="your tax_id",type=str)
@@ -9,9 +10,13 @@ tax_id=args.taxid
 print('Processing tax id:'+args.taxid)
 
 proc_esearch = subprocess.Popen(['esearch', '-db', 'taxonomy', '-query' , tax_id + '[uid]'], stdout=subprocess.PIPE)
+time.sleep(0.7)
 proc_elink = subprocess.Popen(['elink', '-target', 'sra'], stdin=proc_esearch.stdout, stdout=subprocess.PIPE)
+time.sleep(0.7)
 proc_efilter = subprocess.Popen(['efilter', '-query', 'rna seq[stra] AND Transcriptomic[src]'], stdin=proc_elink.stdout, stdout=subprocess.PIPE)
+time.sleep(0.7)
 proc_efetch = subprocess.Popen(['efetch', '-format', 'runinfo', '-mode', 'xml'], stdin=proc_efilter.stdout, stdout=subprocess.PIPE)
+time.sleep(0.7)
 proc_xtract = subprocess.Popen(['xtract', '-pattern', 'Row', '-def', 'N/A','-element', 'Run', 'ReleaseDate', 'LoadDate', 'spots', 'bases', 'spots_with_mates', 'avgLength', 'size_MB', 'download_path', 'Experiment', 'LibraryName', 'LibraryStrategy', 'LibrarySelection', 'LibrarySource', 'LibraryLayout', 'InsertSize', 'InsertDev', 'Platform', 'Model', 'SRAStudy', 'BioProject', 'Study_Pubmed_id', 'ProjectID', 'Sample', 'BioSample', 'SampleType', 'TaxID', 'ScientificName', 'SampleName', 'Sex', 'Tumor', 'Submission', 'Consent', 'RunHash', 'ReadHash' ], stdin=proc_efetch.stdout, stdout=subprocess.PIPE)
 if args.output:
     with open(args.output, 'w') as f:
