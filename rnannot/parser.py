a316c1beed2ba0220a7f6300c43d94888a629051
HsiuKangHuang <k2025242322@gmail.com>
2020-07-22 20:29:32 -0500

    change the default of thread_num to str data type

--- rnannot/parser.py
+++ rnannot/parser.py
@@ -15,6 +15,6 @@ def parse_args(argv):
     parser.add_argument('-a', '--assembly', dest='assembly', default='AssemblyName', help='The assembly name is used for naming output file.')
     parser.add_argument('-t', '--tempFile', dest='tempFile', default=False, action='store_true', help='if specified, intermediate output bam files will be kept')
     parser.add_argument('-m', '--MaximumSRA', dest='MaximumSRA', type=int, default=10, help='The maximum amount of the sra files downloaded from NCBI. The default is 10')
-    parser.add_argument('-T', '--threads_num', dest='threads_num', type=str, default=24, help='The number of threads used in this program. The default is 24')
+    parser.add_argument('-T', '--threads_num', dest='threads_num', type=str, default='24', help='The number of threads used in this program. The default is 24')
     args = parser.parse_args(argv)
     return args
