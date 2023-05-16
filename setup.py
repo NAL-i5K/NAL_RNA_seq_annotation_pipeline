2a47eaf4b48caa7c168f7d7af2142415c37967c1
spcspin <m255360421@gmail.com>
2023-05-16 17:40:41 +0800

    update FastQC, gatk, picard, and Trimmomatic.

--- setup.py
+++ setup.py
@@ -15,13 +15,13 @@ if not exists(lib_dir):
 
 print('Downloading fastQC ...')
 urlretrieve(
-    'https://www.bioinformatics.babraham.ac.uk/projects/fastqc/fastqc_v0.11.7.zip',
-    join(lib_dir, 'fastqc_v0.11.7.zip'))
+    'https://www.bioinformatics.babraham.ac.uk/projects/fastqc/fastqc_v0.12.1.zip',
+    join(lib_dir, 'fastqc_v0.12.1.zip'))
 
 print('Downloading Trimmomatic ...')
 urlretrieve(
-    'http://www.usadellab.org/cms/uploads/supplementary/Trimmomatic/Trimmomatic-0.38.zip',
-    join(lib_dir, 'Trimmomatic-0.38.zip'))
+    'https://github.com/usadellab/Trimmomatic/files/5854859/Trimmomatic-0.39.zip',
+    join(lib_dir, 'Trimmomatic-0.39.zip'))
 
 print('Downloading HISAT2 ...')
 urlretrieve(
@@ -35,12 +35,12 @@ urlretrieve(
 
 print('Downloading Picard ...')
 urlretrieve(
-    'https://github.com/broadinstitute/picard/releases/download/2.18.7/picard.jar',
+    'https://github.com/broadinstitute/picard/releases/download/3.0.0/picard.jar',
     join(lib_dir, 'picard.jar'))
 
 print('Downloading GATK v4 ...')
-urlretrieve('https://github.com/broadinstitute/gatk/releases/download/4.1.6.0/gatk-4.1.6.0.zip',
-    join(lib_dir, 'gatk-4.1.6.0.zip'))
+urlretrieve('https://github.com/broadinstitute/gatk/releases/download/4.4.0.0/gatk-4.4.0.0.zip',
+    join(lib_dir, 'gatk-4.4.0.0.zip'))
 
 print('Downloading bam_to_bigwig ...')
 subprocess.run(['git', 'clone', 'https://github.com/NAL-i5K/bam_to_bigwig.git', join(lib_dir, 'bam_to_bigwig')])
@@ -56,7 +56,7 @@ subprocess.run(['make'])
 chdir(join(project_root))
 
 print('Unpacking fastQC ...')
-with ZipFile(join(lib_dir, 'fastqc_v0.11.7.zip'), 'r') as zip_ref:
+with ZipFile(join(lib_dir, 'fastqc_v0.12.1.zip'), 'r') as zip_ref:
     zip_ref.extractall(lib_dir)
 # fix the permission of fastqc
 chmod(
@@ -64,7 +64,7 @@ chmod(
     S_IXUSR | S_IXOTH | S_IXGRP | S_IRUSR | S_IROTH | S_IRGRP | S_IWUSR)
 
 print('Unpacking Trimmomatic ...')
-with ZipFile(join(lib_dir, 'Trimmomatic-0.38.zip'), 'r') as zip_ref:
+with ZipFile(join(lib_dir, 'Trimmomatic-0.39.zip'), 'r') as zip_ref:
     zip_ref.extractall(lib_dir)
 
 print('Unpacking HISAT2 ...')
@@ -97,14 +97,14 @@ tar.extractall(lib_dir)
 tar.close()
 
 print('Unpacking gatk ...')
-with ZipFile(join(lib_dir, 'gatk-4.1.6.0.zip'), 'r') as zip_ref:
+with ZipFile(join(lib_dir, 'gatk-4.4.0.0.zip'), 'r') as zip_ref:
     zip_ref.extractall(lib_dir)
 
 print('Cleaning the files ...')
 files = [
-    'BBMap_38.00.tar.gz', 'fastqc_v0.11.7.zip',
-    'hisat2-2.1.0-Linux_x86_64.zip', 'Trimmomatic-0.38.zip',
-    'gatk-4.1.6.0.zip'
+    'BBMap_38.00.tar.gz', 'fastqc_v0.12.1.zip',
+    'hisat2-2.1.0-Linux_x86_64.zip', 'Trimmomatic-0.39.zip',
+    'gatk-4.4.0.0.zip'
 ]
 for f in files:
     remove(join(lib_dir, f))
